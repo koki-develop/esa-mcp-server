7c4fe7a3665dd89589affcc6acce6c0feb750a13
koki-develop <kou.pg.0131@gmail.com>
2025-06-19 18:59:52 +0900

    chore: Implement api request method

--- src/lib/esa/index.ts
+++ src/lib/esa/index.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 export class Esa {
   private readonly _teamName: string;
   private readonly _accessToken: string;
@@ -41,5 +43,29 @@ export class Esa {
 
   async deleteComment() {
     // TODO
+  }
+
+  private async _request(params: {
+    path: string;
+    method: string;
+    options?: RequestInit;
+  }): Promise<Response> {
+    const url = new URL("https://api.esa.io");
+    url.pathname = path.join(url.pathname, "/v1", params.path);
+
+    const response = await fetch(url, {
+      method: params.method,
+      headers: {
+        Authorization: `Bearer ${this._accessToken}`,
+        "Content-Type": "application/json",
+      },
+      ...params.options,
+    });
+    if (!response.ok) {
+      const text = await response.text();
+      throw new Error(text);
+    }
+
+    return response;
   }
 }
